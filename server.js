00c027413cc2e1c683cbb1e7d2fb3af5c8d15178
Khue Nguyen <khuezy.nguyen@gmail.com>
2016-08-13 12:00:17 -0700

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var AlexaAppServer = require("./index.js");
 AlexaAppServer.start( {
-  httpsPort: 443,
-  httpsEnabled: true,
+  httpsPort: 443
+  ,httpsEnabled: true
 	//port: process.env.PORT || 8080
 	// Use preRequest to load user data on each request and add it to the request json.
 	// In reality, this data would come from a db or files, etc.
